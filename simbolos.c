6c453b75e09d0ee212e0d8b73f917a50f60a7941
Guilherme Araújo Franca <109182477+FrancaAraujo@users.noreply.github.com>
2024-09-28 12:12:59 -0300

    Nova versão da irrisoria
    Ela já faz a inserção de todas as palavras reservadas no começo para evitar que seja colocado de forma repetida as palavras reservadas dentro da tabela de símbolos, também alterei os dados que são colocados na tabela de símbolos, estava colocando literalmente tudo e acho que da forma que coloquei fica melhor, operadores e abre/fecha parênteses acho que não são necessários na tabela de simbolos, eles devem ser identificados pelo analisador léxico mas acho que não devem ficar na tabela de símbolos.

--- simbolos.c
+++ simbolos.c
@@ -7,53 +7,54 @@
 Simbolo tabela_simbolos[MAX_SIMBOLOS];
 int num_simbolos = 0;
 
-// Função para adicionar um identificador na tabela de símbolos
+// Função para adicionar um identificador na tabela de símbolos (apenas token e lexema)
 void adicionar_identificador(const char *lexema, const char *token)
 {
-    // Adiciona o novo identificador
+    // Verifica se já existe o identificador na tabela de símbolos
+    for (int i = 0; i < num_simbolos; i++)
+    {
+        if (strcmp(tabela_simbolos[i].lexema, lexema) == 0)
+        {
+            return;
+        }
+    }
+
+    // Adiciona o novo identificador (apenas token e lexema)
     strcpy(tabela_simbolos[num_simbolos].lexema, lexema);
     strcpy(tabela_simbolos[num_simbolos].token, token);
     num_simbolos++;
 }
 
-// // Função para buscar um identificador
-// Simbolo *buscar_identificador(const char *nome, const char *escopo) {
-//     for (int i = 0; i < num_simbolos; i++) {
-//         if (strcmp(tabela_simbolos[i].nome, nome) == 0 && strcmp(tabela_simbolos[i].escopo, escopo) == 0) {
-//             return &tabela_simbolos[i];
-//         }
-//     }
-//     return NULL;  // Retorna NULL se não encontrar o identificador
-// }
-
-// Comparison function for qsort
-int compareTokens(const void *a, const void *b)
-{
-    return strcmp(((Simbolo *)a)->token, ((Simbolo *)b)->token);
-}
-
 // Função para exibir a tabela de símbolos
 void exibir_tabela_simbolos()
 {
-    qsort(tabela_simbolos, num_simbolos, sizeof(Simbolo), compareTokens);
     printf("\nTabela de Símbolos:\n");
-    printf("%-30s %-10s\n", "Lexema", "Token");
+    printf("%-15s %-10s\n", "Lexema", "Token");
     for (int i = 0; i < num_simbolos; i++)
     {
-        char *lexema = tabela_simbolos[i].lexema;
-        char *token = tabela_simbolos[i].token;
-
-        if (strcmp(token, "VAZIO") == 0)
-        {
-            printf("%-30s %-10s\n",
-                   " ",
-                   tabela_simbolos[i].token);
-        }
-        else
-        {
-            printf("%-30s %-10s\n",
-                   tabela_simbolos[i].lexema,
-                   tabela_simbolos[i].token);
-        }
+        printf("%-15s %-10s\n", tabela_simbolos[i].lexema, tabela_simbolos[i].token);
     }
+}
+
+// Função para inicializar todas as palavras reservadas na tabela de símbolos
+void inicializar_palavras_reservadas()
+{
+    adicionar_identificador("funcao", "Palavra Reservada");
+    adicionar_identificador("fimfuncao", "Palavra Reservada");
+    adicionar_identificador("chama", "Palavra Reservada");
+    adicionar_identificador("retorna", "Palavra Reservada");
+    adicionar_identificador("tipo", "Palavra Reservada");
+    adicionar_identificador("args", "Palavra Reservada");
+    adicionar_identificador("fimargs", "Palavra Reservada");
+    adicionar_identificador("escreva", "Palavra Reservada");
+    adicionar_identificador("inteiro", "Palavra Reservada");
+    adicionar_identificador("real", "Palavra Reservada");
+    adicionar_identificador("caractere", "Palavra Reservada");
+    adicionar_identificador("se", "Palavra Reservada");
+    adicionar_identificador("entao", "Palavra Reservada");
+    adicionar_identificador("senao", "Palavra Reservada");
+    adicionar_identificador("fimse", "Palavra Reservada");
+    adicionar_identificador("enquanto", "Palavra Reservada");
+    adicionar_identificador("faca", "Palavra Reservada");
+    adicionar_identificador("fimenquanto", "Palavra Reservada");
 }
--- simbolos.h
+++ simbolos.h
@@ -5,18 +5,16 @@
 #define MAX_LEX 50
 #define MAX_TOKEN 50
 
-
 // Estrutura para armazenar os símbolos
-typedef struct {
-    char lexema[MAX_LEX];         // Nome do identificador
-    char token[MAX_TOKEN];         // Nome do identificador
-    // char escopo[10];             // Escopo do identificador (global, local, etc.)
-    // int num_parametros;          // Número de parâmetros (para funções)
+typedef struct
+{
+    char lexema[MAX_LEX];  // Lexema (o nome do identificador)
+    char token[MAX_TOKEN]; // Tipo de token (Identificador, Palavra Reservada, etc.)
 } Simbolo;
 
 // Funções para manipulação da tabela de símbolos
 void adicionar_identificador(const char *lexema, const char *token);
-Simbolo *buscar_identificador(const char *lexema, const char *token);
+Simbolo *buscar_identificador(const char *lexema);
 void exibir_tabela_simbolos();
 
 #endif
